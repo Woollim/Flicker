a0ccb32792e0bcc45890eeaa3e773d0108b06cd3
half-but <qudcks9191@naver.com>
2018-04-11 09:32:16 +0900

    Initial Commit

---
+++ Flicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flicker
+//
+//  Created by 이병찬 on 2018. 4. 11..
+//  Copyright © 2018년 W_Vertex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
